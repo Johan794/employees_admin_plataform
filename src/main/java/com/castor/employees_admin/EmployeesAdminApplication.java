be6ae22a585c8713e031a1406ac32df74c156137
Johan Stiven Ricardo <johan.ricardo1104@gmail.com>
2024-04-02 18:37:19 -0500

    feat: intial code base

---
+++ src/main/java/com/castor/employees_admin/EmployeesAdminApplication.java
@@ -0,0 +1,13 @@
+package com.castor.employees_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castor/employees_admin/EmployeesAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castor.employees_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
